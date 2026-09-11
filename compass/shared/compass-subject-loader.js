108452febb65ed4f3bf1fd4a246d5aba453726b0
Emrys Rowles <110306810+VagabonDigital@users.noreply.github.com>
2026-09-11 14:46:19 -0500

    SIMPLIFY GENERATION AUTHORITY LOAD PATH

--- compass/shared/compass-subject-loader.js
+++ compass/shared/compass-subject-loader.js
@@ -212,16 +212,6 @@
             '../shared/compass-generation-authority.js',
             'Compass generation authority layer could not be loaded.'
         );
-
-        await loadScript(
-            '../shared/compass-generation-authority-hardening.js',
-            'Compass generation authority hardening could not be loaded.'
-        );
-
-        await loadScript(
-            '../shared/compass-generation-authority-surface-lock.js',
-            'Compass generation authority surface lock could not be loaded.'
-        );
     }
 
     async function bootstrap() {
