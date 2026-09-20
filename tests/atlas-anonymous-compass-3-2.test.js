3928edd87aff312d9022544318937f8de0b50576
Emrys Rowles <110306810+VagabonDigital@users.noreply.github.com>
2026-09-20 10:16:19 -0500

    Refresh direct Google auth contract versions

--- tests/atlas-anonymous-compass-3-2.test.js
+++ tests/atlas-anonymous-compass-3-2.test.js
@@ -331,7 +331,7 @@ assert.match(
 
 assert.match(
     originalEntry,
-    /atlas-content-registry\\.js\\?v=20260920-googleauth1/
+    /atlas-content-registry\\.js\\?v=20260920-googleid1/
 );
 
 console.log(
