44611110a48396b8374baea51389463bb37ae021
Emrys Rowles <110306810+VagabonDigital@users.noreply.github.com>
2026-09-24 01:22:56 -0500

    Load finalized Batch 3 SharedWorker client

--- shared/atlas-content-registry.js
+++ shared/atlas-content-registry.js
@@ -1045,7 +1045,7 @@
         }
 
         const src =
-            '/shared/atlas-subject-build-worker-client.js?v=20260924-buildworker2';
+            '/shared/atlas-subject-build-worker-client.js?v=20260924-buildworker3';
 
         if (document.readyState === 'loading') {
             document.write(
