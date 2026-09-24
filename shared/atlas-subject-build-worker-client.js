67888762ae1dfc3b041da9c306b60d60c96d6513
Emrys Rowles <110306810+VagabonDigital@users.noreply.github.com>
2026-09-24 02:28:04 -0500

    Load finalized Batch 4 SharedWorker runtime

--- shared/atlas-subject-build-worker-client.js
+++ shared/atlas-subject-build-worker-client.js
@@ -23,7 +23,7 @@
     }
 
     const WORKER_URL =
-        '/shared/atlas-subject-build-shared-worker.js?v=20260924-buildworker4';
+        '/shared/atlas-subject-build-shared-worker.js?v=20260924-buildworker5';
 
     const WORKER_NAME =
         'atlas-subject-builds';
