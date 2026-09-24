4c0631c9a17754b124b081c546e2de557e9d9c46
Emrys Rowles <110306810+VagabonDigital@users.noreply.github.com>
2026-09-24 11:14:19 -0500

    Shorten locked Compass subject CTA

--- compass/shared/compass-engine.js
+++ compass/shared/compass-engine.js
@@ -17216,7 +17216,7 @@ function updateCoverActionUI() {
     if (button) {
         const label =
             isAnonymousCompassSubjectPreview()
-                ? 'Create free account to explore'
+                ? 'Explore with free account'
                 : myVersionAuthoringOpen
                     ? 'Open subject'
                     : isLessonComplete()
