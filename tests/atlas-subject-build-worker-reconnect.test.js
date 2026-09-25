e06d03e1c9cd771f6af00bb6c3384539d11d930e
Emrys Rowles <110306810+VagabonDigital@users.noreply.github.com>
2026-09-25 00:14:09 -0500

    Align Batch 5 trace contract versions

--- tests/atlas-subject-build-worker-reconnect.test.js
+++ tests/atlas-subject-build-worker-reconnect.test.js
@@ -315,7 +315,7 @@ async function testWorkerReconnectAndAccountSwitch() {
 
     assert.match(
         workers[0].url,
-        /buildworker11/
+        /buildworker12/
     );
 
     Client.enqueueSubject(
@@ -338,7 +338,7 @@ async function testWorkerReconnectAndAccountSwitch() {
         at:
             Date.now(),
         workerVersion:
-            '20260924-buildworker11'
+            '20260924-buildworker12'
     });
 
     workers[0].onerror?.({
@@ -480,7 +480,7 @@ async function testWorkerReconnectAndAccountSwitch() {
             'worker-ready',
         queue: [],
         workerVersion:
-            '20260924-buildworker11'
+            '20260924-buildworker12'
     });
 
     const enqueueCountAfterReady =
